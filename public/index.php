2d5e36a7437019323142fbaa011c46c59d94bf22
forart82 <carmelo.roberto.82@gmail.com>
2019-10-30 21:34:09 +0100

    1 player design

--- public/index.php
+++ public/index.php
@@ -2,19 +2,29 @@
 
 require_once __DIR__ ."/../vendor/autoload.php";
 
-use App\Battlefield;
-use App\Deck;
-use App\Land;
+// use App\Battlefield;
+// use App\Deck;
+// use App\Land;
 
-$battlefied = new Battlefield();
-$deck = new Deck();
-$land = new Land();
+// $battlefied = new Battlefield();
+// $deck = new Deck();
+// $land = new Land();
 
-$battlefield->run();
+// $battlefield->run();
 
 // rooting twig
-$loader = new Twig\Loader\FilesystemLoader('/src/view/index.html.twig');
+$loader = new Twig\Loader\FilesystemLoader(__DIR__.'/../src/view/');
 $twig = new Twig\Environment($loader);
 
 // render
-echo $twig->render('../src/view/index.hmtl.twig', ['lands'=>$land, 'deckPlayerOne'=>$deckPlayerOne, 'deckPlayerTwo'=>$deckPlayerTwo, 'cimeteryPlayerOne'=>$cimeteryPlayerOne, 'cimeteryPlayerTwoe'=>$cimeteryPlayerTwo]);
+// echo $twig->render('../src/view/index.hmtl.twig', 
+// ['lands'=>$land,
+// 'landBackground'=>$landBackground, 
+// 'deckPlayerOne'=>$deckPlayerOne, 
+// 'deckPlayerTwo'=>$deckPlayerTwo, 
+// 'cimeteryPlayerOne'=>$cimeteryPlayerOne, 
+// 'cimeteryPlayerTwoe'=>$cimeteryPlayerTwo,
+// 'mainPlayerOne'=>$mainPlayerOne,
+// 'mainPlayerTwo'=>$mainPlayerTwo]);
+
+echo $twig->render('index.html.twig');

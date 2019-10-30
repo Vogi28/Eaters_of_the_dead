9affc5f750a999868ae46cb975868323fffa375a
NGUYEN Vo Vinh Nghi <vogi.nguyen@live.fr>
2019-10-30 15:29:01 +0100

    function deck land monster

---
+++ src/Monster.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+class Monster 
+{
+
+    public function __construc($card_monsters)
+ {
+     #
+ }
+ 
+ public function setOne_Monster_Card(Type $var = null)
+ {
+     #
+ }
+
+ public function getOne_Monster_Card(Type $var = null)
+ {
+     # 
+ }
+
+ 
+}

d9285698d7dc0cfc39a9a9609ed3baef403f8f20
Hugo Gehl <gehlhugo@gmail.com>
2019-10-30 18:16:22 +0100

    commencé le front

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ ."/../vendor/autoload.php";
+
 use App\Battlefield;
 use App\Deck;
 use App\Land;
@@ -8,6 +10,11 @@ $battlefied = new Battlefield();
 $deck = new Deck();
 $land = new Land();
 
-
 $battlefield->run();
 
+// rooting twig
+$loader = new Twig\Loader\FilesystemLoader('/src/view/index.html.twig');
+$twig = new Twig\Environment($loader);
+
+// render
+echo $twig->render('../src/view/index.hmtl.twig', ['lands'=>$land, 'deckPlayerOne'=>$deckPlayerOne, 'deckPlayerTwo'=>$deckPlayerTwo, 'cimeteryPlayerOne'=>$cimeteryPlayerOne, 'cimeteryPlayerTwoe'=>$cimeteryPlayerTwo]);
